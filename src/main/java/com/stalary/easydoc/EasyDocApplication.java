f03731ff84100146c6ae29e07045759126db75b0
stalary <452024236@qq.com>
2018-09-25 10:14:35 +0800

    init

---
+++ src/main/java/com/stalary/easydoc/EasyDocApplication.java
@@ -0,0 +1,12 @@
+package com.stalary.easydoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyDocApplication.class, args);
+    }
+}
